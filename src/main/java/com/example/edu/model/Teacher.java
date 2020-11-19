486510f1916205ea06325c9b1be50832e98f3f27
work <anisbhati101@gmail.com>
2020-11-19 21:21:33 +0530

    teacher class added

---
+++ src/main/java/com/example/edu/model/Teacher.java
@@ -0,0 +1,41 @@
+package com.example.edu.model;
+
+public class Teacher {
+    private int id;
+    private String name;
+    private String midname;
+    private String lastname;
+    private String mob;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMidname() {
+		return midname;
+	}
+	public void setMidname(String midname) {
+		this.midname = midname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getMob() {
+		return mob;
+	}
+	public void setMob(String mob) {
+		this.mob = mob;
+	}
+    
+    
+}
