ef385437573b87d100649d5706e5339a89ccba54
unknown <anisbhati101@gmail.com>
2020-11-19 21:00:36 +0530

    new class added

---
+++ src/main/java/com/example/edu/model/LoginTracker.java
@@ -0,0 +1,19 @@
+package com.example.edu.model;
+
+public class LoginTracker {
+     private int loginId;
+     private String loginname;
+	public int getLoginId() {
+		return loginId;
+	}
+	public void setLoginId(int loginId) {
+		this.loginId = loginId;
+	}
+	public String getLoginname() {
+		return loginname;
+	}
+	public void setLoginname(String loginname) {
+		this.loginname = loginname;
+	}
+     
+}
