e3e09c07680cd06655bb64b2bb99c71eb2451690
work <anisbhati101@gmail.com>
2020-11-20 12:21:40 +0530

    sib

---
+++ src/main/java/com/example/edu/model/SubClass.java
@@ -0,0 +1,18 @@
+package com.example.edu.model;
+
+public class SubClass {
+    private int id;
+    private String classname;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getClassname() {
+		return classname;
+	}
+	public void setClassname(String classname) {
+		this.classname = classname;
+	}
+}
